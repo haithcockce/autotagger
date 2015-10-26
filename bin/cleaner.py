ae8fa9e28b5ef81c1e14c0b5f8bc7f8ecfee7275
haithcockce <haithcockce@gmail.com>
2015-10-25 22:16:59 -0400

    pushing for posterity

---
+++ bin/cleaner.py
@@ -0,0 +1,64 @@
+import sys
+import csv
+import bs4 as BeautifulSoup
+import string
+import copy
+
+#DATA_DIRECTORY = '/home/dev/School/CSc-522/Project/autotagger/data/original'
+
+#SETUP 
+records = []
+code_only_records = []
+plain_text_records = []
+text_and_html_records = []
+no_html_records = []
+tags= []
+transtable = {ord(c): None for c in string.punctuation}
+PROJECT_PATH = '/home/dev/School/CSc-522/Project/autotagger/'
+stripped = ''
+
+csv_reader = csv.reader(open(PROJECT_PATH + 'data/original/TEST-full.csv', 'r', newline=''))
+for row in csv_reader: 
+    records.append(row)
+
+code_only_records.append(copy.deepcopy(records[0]))
+plain_text_records.append(copy.deepcopy(records[0]))
+
+for i in range(1, len(records)):
+    stripped = ''
+    plain_text_records.append(copy.deepcopy(records[i]))
+    body = BeautifulSoup.BeautifulSoup(plain_text_records[i][8], 'html.parser')
+    if len(body.find_all('code')) != 0:
+        # Gather all the records that have code blocks and unwrap the contents
+        code_only_records.append(copy.deepcopy(records[i]))
+
+        # Remove all code blocks 
+        while len(body.find_all('code')) != 0:
+            body.code.decompose()  
+
+    # Strip away all punctuation and random escape sequences   
+    for dom_element in body.find_all(True):
+        stripped += dom_element.get_text().strip().translate(transtable) + ' '
+        stripped.replace('\n', ' ')
+        stripped.replace('\\n', ' ')
+        stripped.replace('\t', ' ')
+        stripped.replace('\r', ' ')
+    plain_text_records[i][8] = stripped
+
+# Flatten code blocks in code only
+stripped = ''
+for i in len(code_only_records):
+    body = BeautifulSoup.BeautifulSoup(code_only_records[i][8], 'html.parser')
+    code_contents = body.code.contents
+    for i in range(0,len(code_contents)):
+       stripped += code_contents[i] + ' '
+    code_only_records[i][8] = stripped
+
+# Flatten code blocks 
+
+csv_out = open(PROJECT_PATH + 'data/plain-text/TEST-full.csv', 'w', newline='')
+csv_writer = csv.writer(csv_out, dialect='unix')
+for row in plain_text_records:
+    csv_writer.writerow(row)
+
+

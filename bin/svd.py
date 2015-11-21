bb0f792ec271c95e3f10b759069dc8ee2ec20e94
haithcockce <haithcockce@gmail.com>
2015-11-20 22:47:28 -0500

    removing stopwords, using only one tag, and removing lowest 1% of tags

--- bin/svd.py
+++ bin/svd.py
@@ -19,6 +19,7 @@ BODY_INDEX = 1
 TRANSTABLE = {ord(c): ' ' for c in string.punctuation}
 
 unique_monograms = {}
+popular_tags = {}
 unqiue_tags = {}
 records = []
 stopwords = sw.words('english')
@@ -26,20 +27,71 @@ stopwords = sw.words('english')
 start_time = time.time()
 print(start_time)
 
+# most_popular:
+#    param: tags
+#       - array of strings
+#    determine the most popular tag of the 
+#    list of tags provided returning None 
+#    if not found
+def most_popular_tag(tags):
+    for tag in popular_tags:
+        if tag in tags:
+            return tag
+    return None
+
+
+def build_clean_unique_tags(tags_dict):
+
+    # Normalize the counts
+    sigma = float(sum(list(tags_dict.values())))
+    normalized_tags_dict = copy.deepcopy(tags_dict)
+    for key in normalized_tags_dict:
+        normalized_tags_dict[key] = float(normalized_tags_dict[key]) / sigma
+
+    # Remove all tags that show up in 
+    # less than 1% of all tag instances
+    for i in range(0, len(normalized_tags_dict)):
+        key = min(normalized_tags_dict, key=normalized_tags_dict.get)
+        if normalized_tags_dict[key] < .01:
+            normalized_tags_dict.pop(key, None)
+        else:
+            break
+
+    # Build a sorted list of the tags in
+    # order of most common to least common
+    sorted_tags = []
+    for i in range(0, len(normalized_tags_dict)):
+        tag = max(normalized_tags_dict, key=normalized_tags_dict.get)
+        sorted_tags.append(tag)
+        normalized_tags_dict.pop(key, None)
+
+    return sorted_tags
+
+
+
+
+
+
 # Construct the records
 csv_reader = csv.reader(open(FILEPATH, 'r', newline=''))
 for row in csv_reader:
 
     # ignore non-questions
-    if row[1] == '1':
-        # Ignore records that have no tags
-        if len(row[16]) == 0: 
-            continue 
+    if row[1] != '1':
+        continue
 
-        records.append([row[16], row[8]])
+    # Ignore records that have no tags
+    if len(row[16]) == 0:          
+        continue 
+
+    records.append([row[16], row[8]])
+
+
 
 print('DEBUG: Constructed records')
 print(time.time() - start_time)
+
+
 
 for record in records:
     # Construct the dictionary of unique monograms
@@ -49,13 +101,22 @@ for record in records:
         if (word not in unique_monograms) and (word not in stopwords) and (len(word) > 0):
             unique_monograms[word] = 0;
 
-    # Construct the dictionary of unique keys
-    tag_list = 
+# Construct the dictionary of unique keys
+tag_count = {}
+for record in records:
+    tag_list = record[TAGS_INDEX].lower().replace('<', '').replace('>', ' ').split(' ')
+    for tag in tag_list:
+        if tag not in tag_count:
+            tag_count[tag] = 0
+        else:
+            tag_count[tag] += 1
+
+popular_tags = build_clean_unique_tags(tag_count)
 
 
-
-print('DEBUG: Created unique monograms')
+print('DEBUG: Created unique monograms and popular tags')
 print(time.time() - start_time)
+
 
 # After constructing unique monograms,
 # start populating the document matrix
@@ -68,29 +129,28 @@ buff = []
 
 for record in records:
 
+    # if the tag is uncommon, don't use this record
+    tag_list = record[TAGS_INDEX].lower().replace('<', '').replace('>', ' ').split(' ')
+    tag = most_popular_tag(tag_list)
+    if tag == None:
+        continue
+    record[TAGS_INDEX] = tag
+
     # copy the monogram template to count the words for this document 
     monogram_counts = copy.deepcopy(unique_monograms)
     monogram_list = record[BODY_INDEX].lower().translate(TRANSTABLE).split(' ')
-    tag_list = record[TAGS_INDEX].lower().replace('<', '').replace('>', ' ').split(' ')
 
     # begin counting the records. 
     for word in monogram_list:
         if (word not in stopwords) and (len(word) > 0) and (word in monogram_counts):
             monogram_counts[word] += 1
 
-    # for code legibility 
-    #monogram_counts = numpy.array(list(monogram_counts.values()))
-
-     
     # grab the classes (tags) of this record (document) and append
     # to the lists
-    for tag in tag_list:
-        buff.append(list(monogram_counts.values()))
-        #try:
-        #    temp_matrix = numpy.vstack((temp_matrix, monogram_counts))
-        #except NameError:
-        #    temp_matrix = copy.deepcopy(monogram_counts)
-        tags.append(tag)    # This will be used as the classes
+    buff.append(list(monogram_counts.values()))
+    tags.append(tag)    # This will be used as the classes
+
+    # Sanity check and concatenation. Jesus numpy is slow as shit. 
     if (iterations % 250) == 0:
         print('DEBUG: vstack-ed {:d} times'.format(iterations))
         try:
@@ -107,10 +167,8 @@ print(time.time() - start_time)
 sparse_data = spsparse.csc_matrix(temp_matrix)
 
 print('DEBUG: Made sparse matrix')
+print(time.time() - start_time)
 
-end_time = time.time()
-
-print(end_time - start_time)
 
 # At this point in time, there should be the following:
 #   tags: 
@@ -133,35 +191,3 @@ print(end_time - start_time)
 
 
 
-# most_popular:
-#    param: tags
-#       - array of strings
-#    determine the most popular tag of the 
-#    list of tags provided. 
-def most_popular(tags):
-
-    for unique_tag in unique_tags:
-        if unique_tag == tags:
-            return unique_tag
-
-
-def build_clean_unique_tags(tags_dict):
-
-    # Normalize the counts
-    sigma = float(sum(list(tags_dict)))
-    normalized_tags_dict = copy.deepcopy(tags_dict)
-    for key in normalized_tags_dict:
-        normalized_tags_dict[key] = float(normalized_tags_dict[key]) / sigma
-
-        # Remove all tags that show up in 
-        # less than 1% of questions
-        if normalized_tags_dict[key] < 1.0:
-            normalized_tags_dict.pop(key, None)
-
-    sorted_tags = []
-    for i in range(0, len(normalized_tags_dict)):
-        tag = max(normalized_tags_dict, key=normalized_tags_dict.get)
-        sorted_tags.append(tag)
-        normalized_tags_dict.pop(key, None)
-
-    return sorted_tags
